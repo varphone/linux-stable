c1b5789d79c7c0733268589f5ee65475bf5e5d22
Tony Lin <tony.lin@freescale.com>
2011-08-11 17:35:20 +0800

    ENGR00139261 [MX6Q]support 8 bit MMC and eMMC DDR mode
    enable 8 bit MMC mode according to mmc stack.
    enable eMMC DDR mode according to mmc stack, but change
    sdhci a little, since sdhci does not support DDR mode so
    far.
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -274,6 +274,9 @@ struct sdhci_ops {
 	void	(*platform_reset_exit)(struct sdhci_host *host, u8 mask);
 	int	(*set_uhs_signaling)(struct sdhci_host *host, unsigned int uhs);
 
+	void		(*pre_tuning)(struct sdhci_host *host, u32 val);
+	int		(*platform_ddr_mode)(struct sdhci_host *host,
+					       int mode);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
