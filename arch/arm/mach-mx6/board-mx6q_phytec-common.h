8de79b6919f923f21db0fc3c9525a0feb7b1b1ed
Dmitry Lavnikevich <d.lavnikevich@sam-solutions.net>
2012-12-28 15:57:29 +0300

    Move common code to separate file. Adding Unique ID reading for bouth boards.

---
+++ arch/arm/mach-mx6/board-mx6q_phytec-common.h
@@ -0,0 +1,8 @@
+#ifndef __BOARD_MX6Q_PHYTEC_COMMON_H__
+#define __BOARD_MX6Q_PHYTEC_COMMON_H__
+
+#include <mach/iomux-mx6q.h>
+
+extern __init void mx6_setup_cpuinfo(void);
+
+#endif /* __BOARD_MX6Q_PHYTEC_COMMON_H__ */
