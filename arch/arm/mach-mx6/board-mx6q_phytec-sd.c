377fb711acc49f5387a8e7f4aeedb50ae632beb0
Christian Hemp <c.hemp@phytec.de>
2013-10-04 14:03:11 +0200

    imx6: phyFLEX: add support for PCM-958 module
    Add support for the phytec WLAN module.

--- arch/arm/mach-mx6/board-mx6q_phytec-sd.c
+++ arch/arm/mach-mx6/board-mx6q_phytec-sd.c
@@ -27,6 +27,21 @@
 #include "cpu_op-mx6.h"
 #include "board-mx6q_phytec-sd.h"
 
+#if defined(CONFIG_WL12XX)
+	#include <linux/delay.h>
+	#include <linux/wl12xx.h>
+	#include <linux/gpio.h>
+
+	#include <linux/regulator/anatop-regulator.h>
+	#include <linux/regulator/consumer.h>
+	#include <linux/regulator/machine.h>
+	#include <linux/regulator/fixed.h>
+
+	#define WLAN_IRQ	IMX_GPIO_NR(6, 17)
+	#define WLAN_ENABLE	IMX_GPIO_NR(6, 18)
+#endif
+
+
 static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING(3, 50);
 static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING(3, 100);
 static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING(3, 200);
@@ -154,25 +169,82 @@ static int plt_sd3_pad_change(unsigned int index, int clock)
 
 #if defined(CONFIG_MACH_MX6Q_PHYFLEX)
 static struct esdhc_platform_data mx6_phytec_sd2_data __initconst = {
-//	.cd_gpio		= 0,
-//	.wp_gpio		= 0,
 	.support_18v		= 0,
-	.support_8bit		= 1,
+	.support_8bit		= 0,
 	.keep_power_at_suspend	= 1,
 	.delay_line		= 0,
 	.platform_pad_change	= plt_sd2_pad_change,
 };
 #endif
 
+#if !defined(CONFIG_WL12XX)
 static struct esdhc_platform_data mx6_phytec_sd3_data __initconst = {
-//	.cd_gpio		= 0,
-//	.wp_gpio		= 0,
 	.support_18v		= 0,
-	.support_8bit		= 1,
+	.support_8bit		= 0,
 	.keep_power_at_suspend	= 1,
 	.delay_line		= 0,
 	.platform_pad_change	= plt_sd3_pad_change,
 };
+
+#else
+static struct esdhc_platform_data mx6_phytec_sd3_data __initconst = {
+	.always_present		= 1,
+	.cd_gpio		= -1,
+	.wp_gpio		= -1,
+	.support_18v		= 0,
+	.support_8bit		= 0,
+	.keep_power_at_suspend	= 0,
+	.caps			= MMC_CAP_POWER_OFF_CARD,
+	.platform_pad_change	= plt_sd3_pad_change,
+};
+
+static void wl1271_set_power(bool enable)
+{
+	if (0 == enable) {
+		gpio_set_value(WLAN_ENABLE, 0);     /* momentarily disable */
+		mdelay(2);
+		gpio_set_value(WLAN_ENABLE, 1);
+	}
+}
+
+struct wl12xx_platform_data n6q_wlan_data __initdata = {
+	.irq		= gpio_to_irq(WLAN_IRQ),
+	.board_ref_clock = WL12XX_REFCLOCK_38, /* 38.4 MHz */
+	.set_power	= wl1271_set_power,
+};
+
+static struct regulator_consumer_supply n6q_vwl1271_consumers[] = {
+	REGULATOR_SUPPLY("vmmc", "sdhci-esdhc-imx.1"),
+};
+
+static struct regulator_init_data n6q_vwl1271_init = {
+	.constraints = {
+		.name		= "VDD_1.8V",
+		.valid_ops_mask	= REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies	= ARRAY_SIZE(n6q_vwl1271_consumers),
+	.consumer_supplies	= n6q_vwl1271_consumers,
+};
+
+static struct fixed_voltage_config n6q_vwl1271_reg_config = {
+	.supply_name	= "vwl1271",
+	.microvolts	= 1800000, /* 1.80V */
+	.gpio		= -1,
+	.startup_delay	= 70000, /* 70ms */
+	.enable_high	= 1,
+	.enabled_at_boot = 0,
+	.init_data	= &n6q_vwl1271_init,
+};
+
+static struct platform_device n6q_vwl1271_reg_devices = {
+	.name	= "reg-fixed-voltage",
+	.id	= 4,
+	.dev	= {
+		.platform_data = &n6q_vwl1271_reg_config,
+	},
+};
+
+#endif
 
 void __init board_esdhc_init(char id, int cd_gpio, int wp_gpio)
 {
@@ -183,9 +255,21 @@ void __init board_esdhc_init(char id, int cd_gpio, int wp_gpio)
 		imx6q_add_sdhci_usdhc_imx(1, &mx6_phytec_sd2_data);
 	} else
 #endif
+
 	if (2 == id) {
+#if !defined(CONFIG_WL12XX)
 		mx6_phytec_sd3_data.cd_gpio = cd_gpio;
 		mx6_phytec_sd3_data.wp_gpio = wp_gpio;
 		imx6q_add_sdhci_usdhc_imx(2, &mx6_phytec_sd3_data);
+#else
+		gpio_request(WLAN_ENABLE, "WLAN Enable");
+		gpio_direction_output(WLAN_ENABLE, 1);
+		wl1271_set_power(0);
+		imx6q_add_sdhci_usdhc_imx(2, &mx6_phytec_sd3_data);
+		/* WL12xx WLAN Init */
+		if (wl12xx_set_platform_data(&n6q_wlan_data))
+		      pr_err("error setting wl12xx data\n");
+		platform_device_register(&n6q_vwl1271_reg_devices);
+#endif
 	}
 }
