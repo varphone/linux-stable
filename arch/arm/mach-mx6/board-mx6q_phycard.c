e1e04675095d326b66b26e66bca31db550745081
Christian Hemp <c.hemp@phytec.de>
2014-03-20 13:47:13 +0100

    phyCARD-i.MX6: Remove sec IPU and set display mode
    On phyCARD we have only one display connector. So we can remove the second
    display channel and set to Single Channel mode.
    
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- arch/arm/mach-mx6/board-mx6q_phycard.c
+++ arch/arm/mach-mx6/board-mx6q_phycard.c
@@ -369,10 +369,7 @@ static struct fsl_mxc_ldb_platform_data ldb_data = {
 	.ipu_id		= 0,
 	.disp_id	= 0,
 	.ext_ref	= 1,
-	//.mode		= LDB_SIN0,
-	.mode		= LDB_SEP0,
-	.sec_ipu_id	= 1,
-	.sec_disp_id	= 1,
+	.mode		= LDB_SIN0,
 	.modes		= ldb_modedb,
 	.num_modes	= ARRAY_SIZE(ldb_modedb),
 };
