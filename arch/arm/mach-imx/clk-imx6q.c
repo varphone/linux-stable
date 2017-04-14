b8d7f8ee44ed3fd22d69176883e774f69d3e499e
黄华锋 <varphone@qq.com>
2017-04-14 09:10:59 +0800

    ARM: imx6q: Add RMII support for myimx6ek200
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- arch/arm/mach-imx/clk-imx6q.c
+++ arch/arm/mach-imx/clk-imx6q.c
@@ -897,8 +897,13 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 		imx_clk_prepare_enable(clk[IMX6QDL_CLK_USBPHY2_GATE]);
 	}
 
-	/*Set enet_ref clock to 125M to supply for RGMII tx_clk */
-	clk_set_rate(clk[IMX6QDL_CLK_ENET_REF], 125000000);
+	if (of_machine_is_compatible("myzr,myimx6ek200")) {
+		/* Set enet_ref clock to 50M to supply for RMII tx_clk */
+		clk_set_rate(clk[IMX6QDL_CLK_ENET_REF], 50000000);
+	} else {
+		/* Set enet_ref clock to 125M to supply for RGMII tx_clk */
+		clk_set_rate(clk[IMX6QDL_CLK_ENET_REF], 125000000);
+	}
 
 #ifdef CONFIG_MX6_VPU_352M
 	/*
