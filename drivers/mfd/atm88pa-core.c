9608e4f12a8e7e1baf2cf9e9d464dcab6b21b792
黄华锋 <varphone@qq.com>
2017-06-27 18:25:27 +0800

    mfd: Add support for Atmel Microelectronics ATMEGA88PA with I2C
    Signed-off-by: 黄华锋 <varphone@qq.com>

---
+++ drivers/mfd/atm88pa-core.c
@@ -0,0 +1,379 @@
+/*
+ * drivers/mfd/atm88pa-core.c
+ *
+ * Core of ATMEGA88PA MFD driver
+ *
+ * Copyright (C) 2017 NanJing No.55 Research Institute Technology Development CO.,LTD.
+ *
+ * Author: Varphone Wong <varphone@qq.com>
+ *
+ */
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/interrupt.h>
+#include <linux/input.h>
+#include <linux/leds.h>
+#include <linux/pm.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
+#include <linux/slab.h>
+#include <linux/mfd/atm88pa.h>
+#include <linux/mfd/atm88pa-private.h>
+
+int atm88pa_read(struct atm88pa *atm, u8 reg)
+{
+	return i2c_smbus_read_byte_data(atm->i2c, reg);
+}
+
+int atm88pa_read_word(struct atm88pa *atm, u8 reg)
+{
+	return i2c_smbus_read_word_data(atm->i2c, reg);
+}
+
+int atm88pa_write(struct atm88pa *atm, u8 reg, u8 val)
+{
+	return i2c_smbus_write_byte_data(atm->i2c, reg, val);
+}
+
+int atm88pa_write_word(struct atm88pa *atm, u8 reg, u16 val)
+{
+	return i2c_smbus_write_word_data(atm->i2c, reg, val);
+}
+
+int atm88pa_get_sw_ver(struct atm88pa *atm)
+{
+	return atm88pa_read_word(atm, ATM88PA_REG_SW_VER);
+}
+
+#ifdef CONFIG_OF
+static int atm88pa_parse_dt(struct atm88pa *atm)
+{
+	int i, ret;
+	struct device_node *np = atm->dev->of_node;
+	struct device_node *keypad = of_get_child_by_name(np, "keypad");
+	struct device_node *leds = of_get_child_by_name(np, "leds");
+	struct device_node *power = of_get_child_by_name(np, "power");
+	struct device_node *child = NULL;
+	struct led_classdev *led;
+	enum of_gpio_flags flags;
+
+	/* Properties of keypad/... */
+	ret = of_property_read_u32(keypad, "debounce_time", &atm->keypad.debounce_time);
+	if (ret) {
+		dev_err(atm->dev, "debounce_time missing or invalid\n");
+		return ret;
+	}
+	ret = of_property_read_u32(keypad, "repeat", &atm->keypad.repeat);
+	if (ret) {
+		dev_err(atm->dev, "repeat missing or invalid\n");
+		return ret;
+	}
+	ret = of_property_read_u16_array(keypad, "keymap", atm->keypad.keymap,
+	                                 ARRAY_SIZE(atm->keypad.keymap));
+	if (ret) {
+		dev_err(atm->dev, "keymap missing or invalid\n");
+		return ret;
+	}
+
+	/* Properties of leds/... */
+	for_each_child_of_node(leds, child) {
+		ret = of_property_read_u32(child, "id", &i);
+		if (ret || i < 0 || i > ARRAY_SIZE(atm->leds)) {
+			dev_err(atm->dev, "wrong LED id number\n");
+			return -EINVAL;
+		}
+
+		led = &atm->leds[i];
+
+		ret = of_property_read_string(child, "label", &led->name);
+		if (ret) {
+			if (child->name == NULL) {
+				dev_err(atm->dev, "label missing or invalid\n");
+				return ret;
+			}
+			led->name = child->name;
+		}
+		ret = of_property_read_u32(child, "brightness", &led->brightness);
+		if (ret)
+			atm->leds[i].brightness = LED_OFF;
+
+		ret = of_property_read_u32(child, "max-brightness", &led->max_brightness);
+		if (ret)
+			atm->leds[i].max_brightness = LED_FULL;
+
+		of_property_read_string(child, "default-trigger", &led->default_trigger);
+	}
+
+	/* Properties of power/... */
+	atm->power.gpio = of_get_gpio_flags(power, 0, &flags);
+	atm->power.active_low = flags & OF_GPIO_ACTIVE_LOW;
+
+	return 0;
+}
+#else
+static inline int atm88pa_parse_dt(struct device *dev, struct atm88pa *atm)
+{
+	return -ENODEV;
+}
+#endif
+
+static ssize_t atm88pa_get_status_attr(struct device *dev,
+				       struct device_attribute *attr,
+				       char *buf)
+{
+	struct atm88pa *atm = miscdev_to_atm88pa(dev);
+	int status;
+
+	status = atm88pa_read(atm, ATM88PA_REG_STATUS);
+
+	return scnprintf(buf, PAGE_SIZE, "0x%02x\n", status);
+}
+
+static ssize_t atm88pa_get_version_attr(struct device *dev,
+					struct device_attribute *attr,
+					char *buf)
+{
+	struct atm88pa *atm = miscdev_to_atm88pa(dev);
+	int version;
+
+	version = atm88pa_get_sw_ver(atm);
+
+	return scnprintf(buf, PAGE_SIZE, "0x%04x\n", version);
+}
+
+
+DEVICE_ATTR(status, S_IRUGO, atm88pa_get_status_attr, NULL);
+DEVICE_ATTR(version, S_IRUGO, atm88pa_get_version_attr, NULL);
+
+static struct attribute *atm88pa_attrs[] = {
+	&dev_attr_status.attr,
+	&dev_attr_version.attr,
+	NULL
+};
+
+static const struct attribute_group atm88pa_attr_group = {
+	.attrs = atm88pa_attrs,
+};
+
+static const struct attribute_group *atm88pa_attr_groups[] = {
+	&atm88pa_attr_group,
+	NULL,
+};
+
+static int atm88pa_misc_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int atm88pa_misc_release(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static long atm88pa_misc_ioctl(struct file *file, unsigned int cmd,
+			       unsigned long arg)
+{
+	return 0;
+}
+
+static struct file_operations atm88pa_misc_fops = {
+	.owner		= THIS_MODULE,
+	.open		= atm88pa_misc_open,
+	.release	= atm88pa_misc_release,
+	.unlocked_ioctl	= atm88pa_misc_ioctl,
+};
+
+/*
+static struct miscdevice atm88pa_misc = {
+	.name	= "atm88pa",
+	.minor	= MISC_DYNAMIC_MINOR,
+	.fops	= &atm88pa_misc_fops,
+};
+*/
+
+static int atm88pa_misc_register(struct atm88pa *atm)
+{
+	int ret;
+	struct miscdevice *misc = &atm->misc;
+
+	misc->name	= "atm88pa";
+	misc->minor	= MISC_DYNAMIC_MINOR;
+	misc->fops	= &atm88pa_misc_fops;
+	misc->groups	= atm88pa_attr_groups;
+	misc->parent	= atm->dev;
+
+	ret = misc_register(misc);
+	if (ret) {
+		dev_err(atm->dev, "misc: %s register failed, err: %d\n",
+			misc->name, ret);
+		return ret;
+	}
+
+	dev_info(atm->dev, "misc: %s registered.\n", misc->name);
+
+	return 0;
+}
+
+static int atm88pa_misc_unregister(struct atm88pa *atm)
+{
+	int ret;
+	struct miscdevice *misc = &atm->misc;
+	
+	ret = misc_deregister(misc);
+	if (ret) {
+		dev_err(atm->dev, "misc: %s unregister failed, err: %d\n",
+			misc->name, ret);
+		return ret;
+	}
+
+	dev_info(atm->dev, "misc: %s unregistered.\n", misc->name);
+
+	return 0;
+}
+
+static int atm88pa_probe(struct i2c_client* client,
+			 const struct i2c_device_id* id)
+{
+	int err;
+	struct atm88pa *atm;
+
+	/* Allocate resource */
+	atm = devm_kzalloc(&client->dev, sizeof(*atm), GFP_KERNEL);
+	if (!atm)
+		return -ENOMEM;
+
+	i2c_set_clientdata(client, atm);
+
+	atm->dev = &client->dev;
+	atm->i2c = client;
+
+#if 0
+	/* Check software version */
+	if (atm88pa_get_sw_ver(atm) != 0x0001) {
+		dev_err(atm->dev, "ATMEGA88PA not found.\n");
+		return -ENODEV;
+	}
+#endif
+	err = atm88pa_parse_dt(atm);
+	if (err) {
+		dev_err(atm->dev, "DT not found\n");
+		return err;
+	}
+
+	err = atm88pa_misc_register(atm);
+	if (err) {
+		goto misc_error;
+	}
+
+	err = atm88pa_keypad_register(atm);
+	if (err) {
+		dev_err(atm->dev, "register keypad subsystem failed, err: %d\n", err);
+		goto keypad_error;
+	}
+
+	err = atm88pa_leds_register(atm);
+	if (err) {
+		dev_err(atm->dev, "register leds subsystem failed, err: %d\n", err);
+		goto leds_error;
+	}
+
+	err = atm88pa_power_register(atm);
+	if (err) {
+		dev_err(atm->dev, "register power subsystem failed, err: %d\n", err);
+		goto power_error;
+	}
+
+	err = atm88pa_irq_init(atm);
+	if (err) {
+		dev_err(atm->dev, "initialize irq failed, err: %d\n", err);
+		goto irq_error;
+	}
+
+	dev_info(atm->dev, "probed.\n");
+
+	return 0;
+
+irq_error:
+	atm88pa_power_unregister(atm);
+power_error:
+	atm88pa_leds_unregister(atm);
+leds_error:
+	atm88pa_keypad_unregister(atm);
+keypad_error:
+	atm88pa_misc_unregister(atm);
+misc_error:
+	return err;
+}
+
+static int atm88pa_remove(struct i2c_client *client)
+{
+	struct atm88pa *atm = i2c_get_clientdata(client);
+
+	atm88pa_irq_exit(atm);
+	atm88pa_misc_unregister(atm);
+	atm88pa_keypad_unregister(atm);
+	atm88pa_leds_unregister(atm);
+	atm88pa_power_unregister(atm);
+
+	dev_info(&client->dev, "removed.\n");
+
+	return 0;
+}
+
+#ifdef CONFIG_PM_SLEEP
+static int atm88pa_suspend(struct device *dev)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct atm88pa *atm = i2c_get_clientdata(client);
+
+	return atm88pa_irq_suspend(atm);
+}
+
+static int atm88pa_resume(struct device *dev)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct atm88pa *atm = i2c_get_clientdata(client);
+
+	return atm88pa_irq_resume(atm);
+}
+#endif
+
+static SIMPLE_DEV_PM_OPS(atm88pa_pm_ops,
+                         atm88pa_suspend,
+                         atm88pa_resume);
+
+static const struct i2c_device_id atm88pa_id[] = {
+	{ "atm88pa", 0 },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(i2c, atm88pa_id);
+
+#ifdef CONFIG_OF
+static const struct of_device_id atm88pa_of_match[] = {
+	{ .compatible = "tdc,atm88pa", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, atm88pa_of_match);
+#endif
+
+static struct i2c_driver atm88pa_driver = {
+	.driver = {
+		.name           = "atm88pa",
+		.owner          = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = of_match_ptr(atm88pa_of_match),
+#endif
+		.pm             = &atm88pa_pm_ops,
+	},
+	.id_table       = atm88pa_id,
+	.probe          = atm88pa_probe,
+	.remove         = atm88pa_remove,
+};
+
+module_i2c_driver(atm88pa_driver);
+
+MODULE_AUTHOR("Varphone Wong <varphone@qq.com>");
+MODULE_DESCRIPTION("ATMEGA88PA MFD driver");
+MODULE_LICENSE("GPL");
+
