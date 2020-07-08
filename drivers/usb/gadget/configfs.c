dc30d0aed533051f47f07ab85fe5663a2461fbf2
Yijun Zhou <yijunz@nvidia.com>
2020-07-08 13:20:36 +0800

    usb: use compatible configfs ops w/ and w/o CONFIG_USB_CONFIGFS_UEVENT
    Nxp has fix (commit a6af4089638e3376a515ea3efc789ed48040c860) in kernel
    upgrade 4.4.227, which has conflict against current implementation from
    NV CONFIG_USB_CONFIGFS_UEVENT, hence breaking usb functionality and
    stress test.
    Revert back to CONFIG_USB_CONFIGFS_UEVENT previous version, using macro
    to isolate different build flag.
    
    Bug 200625526
    
    Change-Id: I2bf63d17b00b9d908cc387ae5a8df43dd08463c1
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.4/+/2372126
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Vijay Mali <vmali@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Yijun Zhou <yijunz@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -735,7 +735,6 @@ static struct config_group *config_desc_make(
 	ret = usb_add_config_only(&gi->cdev, &cfg->c);
 	if (ret)
 		goto err;
-
 	return &cfg->group;
 err:
 	kfree(cfg->c.label);
@@ -1552,6 +1551,7 @@ static void configfs_composite_unbind(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
+#ifndef CONFIG_USB_CONFIGFS_UEVENT
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
 {
@@ -1642,6 +1642,7 @@ static void configfs_composite_resume(struct usb_gadget *gadget)
 	composite_resume(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
+#endif
 
 #ifdef CONFIG_USB_CONFIGFS_UEVENT
 static int android_setup(struct usb_gadget *gadget,
@@ -1708,20 +1709,17 @@ static const struct usb_gadget_driver configfs_driver_template = {
 	.setup          = android_setup,
 	.reset          = android_disconnect,
 	.disconnect     = android_disconnect,
-#else
-	.setup          = composite_setup,
-	.reset          = composite_disconnect,
-	.disconnect     = composite_disconnect,
-#endif
+
 	.suspend	= composite_suspend,
 	.resume		= composite_resume,
-
+#else
 	.setup          = configfs_composite_setup,
 	.reset          = configfs_composite_disconnect,
 	.disconnect     = configfs_composite_disconnect,
 
 	.suspend	= configfs_composite_suspend,
 	.resume		= configfs_composite_resume,
+#endif
 
 	.max_speed	= USB_SPEED_SUPER,
 	.driver = {
