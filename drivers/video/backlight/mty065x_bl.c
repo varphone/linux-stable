c253788d4d9d58b5b65db236065599e34a18daa2
Varphone Wong <varphone@qq.com>
2021-03-30 14:46:05 +0800

    backlight: mty065x_bl: refactor version guard
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- drivers/video/backlight/mty065x_bl.c
+++ drivers/video/backlight/mty065x_bl.c
@@ -802,9 +802,7 @@ static int mty065x_configure(struct mty065x *mty)
 		mty065x_set_input_format(mty);
 		/* Input Source Select must be the last to apply the settings */
 		mty065x_set_input_select_value(mty, 1);
-#ifdef CONFIG_BACKLIGHT_MTY065X_V2
-
-#else
+#if !defined(CONFIG_BACKLIGHT_MTY065X_V2)
 		mty065x_set_test_pattern(mty);
 #endif
 
@@ -822,9 +820,7 @@ static int mty065x_configure(struct mty065x *mty)
 		mty065x_set_input_format(mty);
 		/* Input Source Select must be the last to apply the settings */
 		mty065x_set_input_select(mty);
-#ifdef CONFIG_BACKLIGHT_MTY065X_V2
-
-#else
+#if !defined(CONFIG_BACKLIGHT_MTY065X_V2)
 		mty065x_set_test_pattern_value(mty, 0);
 #endif
 	}
